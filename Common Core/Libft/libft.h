a7c136e8e9a08879ffbccb8fe4a6446f7621bddf
Mateusz Galecki <mgalecki@c2r7s1.42warsaw.pl>
2024-03-06 18:35:20 +0100

    First commit

---
+++ Common Core/Libft/libft.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mgalecki <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/06 18:01:49 by mgalecki          #+#    #+#             */
+/*   Updated: 2024/03/06 18:03:10 by mgalecki         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+#include <stdlib.h>
+#include <unistd.h>
+
+int ft_isalpha(int c);
+int ft_isdigit(int c);
+int	ft_isalnum(int c);
+int	ft_isascii(int c);
+int	ft_isprint(int c);
+size_t	ft_strlen(char const *str);
+
+#endif
