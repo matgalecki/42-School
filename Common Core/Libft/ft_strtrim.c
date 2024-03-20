96b18b505de1e29848d42e829f7bbb8d8b39c83b
Mateusz Galecki <mgalecki@c2r7s3.42warsaw.pl>
2024-03-20 21:15:38 +0100

    modify files: Makefile, libft.h, ft_strtrim.c and add file ft_split.c

--- Common Core/Libft/ft_strtrim.c
+++ Common Core/Libft/ft_strtrim.c
@@ -11,6 +11,20 @@
 /* ************************************************************************** */
 #include "libft.h"
 
+static int	checkifset(char const c, char const *set)
+{
+	int	i;
+
+	i = 0;
+	while (set[i])
+	{
+		if (set[i] == c)
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
 char	*ft_strtrim(char const *s1, char const *set)
 {
 	char	*str;
@@ -18,9 +32,42 @@ char	*ft_strtrim(char const *s1, char const *set)
 
 	if (!s1 || !set)
 		return (NULL);
-
-	sstr = ft_strlen(s1) - ft_strlen(set) + 1;  
-	str = (char *)malloc(sstr * sizeof(char));
+	while (*s1)
+	{
+		if (checkifset(((char)*s1), set) == 1)
+			s1++;
+		else
+			break ;
+	}
+	size = ft_strlen(s1);
+	while (size != 0)
+	{
+		if (checkifset(s1[size - 1], set) == 1)
+			size--;
+		else
+			break ;
+	}
+	str = (char *)malloc(size * sizeof(char) + 1);
 	if (!str)
 		return (NULL);
+	ft_strlcpy(str, (char *)s1, size +1);
+	return (str);
 }
+/* 
+#include <stdio.h>
+int main()
+{
+	char *s = "asd";
+	char *st = "d";
+	char *result = ft_strtrim(s, st);
+	if (result)
+	{
+		printf("%s", result);
+		free(result);
+	}
+	else
+		printf("Memory allocation error");
+	return (0);
+	 
+}
+ */
