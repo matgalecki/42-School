44f18c335666dbf044b377c45ae03e43a3b56d67
Mateusz Galecki <mgalecki@c2r8s3.42warsaw.pl>
2024-03-21 21:40:06 +0100

    modify ft_split.c file

--- Common Core/Libft/ft_split.c
+++ Common Core/Libft/ft_split.c
@@ -13,8 +13,47 @@
 
 char	**ft_split(char const *s, char c)
 {
-	char **str;
+	char 	**result;
+	int		word_s;
+	int		i;
+	int		j;
 
+	ft_set_vars(&i, &j, si);
+	result = ft_calloc((ft_word_count(s, c) + 1), sizeof(char *));
+	if (!result)
+		return (NULL);
+	while (i <= ft_strlen(s))
+	{
+	}
+}
+
+static void	ft_set_vars(size_t *i, int *j, int *word_s)
+{
+	*i = 0;
+	*j = 0;
+	*word_s = -1;
+}
+
+static int	ft_word_count(const char *str, char c)
+{
+	int	count;
+	int	flag;
+
+	count = 0;
+	flag = 0;
+
+	while(*str)
+	{
+		if (*str != c && flag == 0)
+		{	
+			flag = 1;
+			count++;
+		}
+		else if (*str == c)
+			flag = 0;
+		str++
+	}
+	return (count);
 }
 /* 
 int main ()
